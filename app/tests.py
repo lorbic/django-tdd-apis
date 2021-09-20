41eb8222d0799b0aa28c4eca11beaa555ebbbf07
Vikash Patel <vikaspatelp83@gmail.com>
2021-09-20 23:54:48 +0530

    Add more tests

--- app/tests.py
+++ app/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from app.calc import add
+from app.calc import add, sub
 
 
 class CalcTests(TestCase):
@@ -8,4 +8,11 @@ class CalcTests(TestCase):
     def test_add_numbers(self):
         "Test that two number are added togerther"
         self.assertEqual(add(3, 8), 11)
-        
+        self.assertEqual(add(-3, -8), -11)
+    
+    def test_subtract_numbers(self):
+        """Test that the first number is subtracted from second number"""
+        self.assertEqual(sub(3, 8), 5)
+        self.assertEqual(sub(-5, 5), 10)
+
+   
