d279f2bb0834561825782281c24208e5e4ade11d
Vikash Patel <vikaspatelp83@gmail.com>
2021-09-20 23:29:27 +0530

    Add sample unit tests

---
+++ app/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from app.calc import add
+
+
+class CalcTests(TestCase):
+
+    def test_add_numbers(self):
+        "Test that two number are added togerther"
+        self.assertEqual(add(3, 8), 11)
+        
