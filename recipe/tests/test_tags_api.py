1f3c1093966913753106ec4755ce52087c7ae7ae
Vikash <vikaspatelp83@gmail.com>
2021-10-20 16:57:57 +0530

    Fix linting issues

--- recipe/tests/test_tags_api.py
+++ recipe/tests/test_tags_api.py
@@ -65,4 +65,3 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
-
